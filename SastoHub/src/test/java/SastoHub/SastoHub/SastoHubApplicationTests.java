3ea9b03d9a1bb30430a3d2030e01d4990e387224
avi.13 <avicekbhatta.123@gmail.com>
2022-12-18 10:14:14 +0545

    projectStructure

---
+++ SastoHub/src/test/java/SastoHub/SastoHub/SastoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package SastoHub.SastoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SastoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
