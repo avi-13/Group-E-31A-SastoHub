275d42905cdef1eaa21fcbc52f14c33ef1bcd55c
avi.13 <avicekbhatta.123@gmail.com>
2022-12-26 19:40:26 +0545

    structures

---
+++ SastoHub/src/main/java/SastoHub/SastoHub/SastoHubApplication.java
@@ -0,0 +1,13 @@
+package SastoHub.SastoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SastoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SastoHubApplication.class, args);
+	}
+
+}
