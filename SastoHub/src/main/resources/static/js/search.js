2ed312e8de308801a679b5b44f19d734a07a1155
aryalaashu <aryalaashutosh@gmail.com>
2022-12-28 20:22:35 +0545

    Function Added in JS

---
+++ SastoHub/src/main/resources/static/js/search.js
@@ -0,0 +1,7 @@
+function myFunction() {
+      
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
