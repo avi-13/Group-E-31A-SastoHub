e4074692fab191df1de1fdd279edd4316ab8115c
avi.13 <avicekbhatta.123@gmail.com>
2023-01-08 12:49:53 +0545

    database done addproduct

--- SastoHub/src/main/resources/static/js/productphoto.js
+++ SastoHub/src/main/resources/static/js/productphoto.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("#foram"),
+const form = document.querySelector("form"),
 fileInput = document.querySelector(".file-input"),
 progressArea = document.querySelector(".progress-area"),
 uploadedArea = document.querySelector(".uploaded-area");
