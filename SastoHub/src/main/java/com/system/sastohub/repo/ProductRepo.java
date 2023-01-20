7cd96897b8e8080d4da383ec74f825527b69784a
ImSuis <joshisuyash322@gmail.com>
2023-01-20 12:00:30 +0545

    added validation

--- SastoHub/src/main/java/com/system/sastohub/repo/ProductRepo.java
+++ SastoHub/src/main/java/com/system/sastohub/repo/ProductRepo.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepo extends JpaRepository < Product, Integer>{
-    Optional<Product> findProductByName(String name);
+//    Optional<Product> findProductByName(String name);
 }
 
 
