9b5134485b14d3c457e7c04024ef9d303e22837c
avi.13 <avicekbhatta.123@gmail.com>
2023-01-23 20:04:47 +0545

    viewing product by admin

--- SastoHub/src/main/java/com/system/sastohub/controller/AdminController.java
+++ SastoHub/src/main/java/com/system/sastohub/controller/AdminController.java
@@ -1,20 +1,63 @@
 package com.system.sastohub.controller;
 
+import com.system.sastohub.entity.Product;
+import com.system.sastohub.services.ProductService;
 import com.system.sastohub.services.UserServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/productlist")
+    private final ProductService productService;
+
+    @GetMapping("/product")
     public String homePage() {
         return "adminproductlist";
     }
 
 
+
+    @GetMapping("/list")
+    public String getproductList(Model model){
+        List<Product> adminproduct=productService.fetchAll();
+        model.addAttribute("product", adminproduct.stream().map(product ->
+                Product.builder()
+                        .productId(product.getProductId())
+                        .imageBase64(getImageBase64(product.getImage()))
+                        .productTitle(product.getProductTitle())
+                        .productCategory(product.getProductCategory())
+                        .productDescription(product.getProductDescription())
+                        .productPrice(product.getProductPrice())
+                        .build()
+
+        ));
+        return "adminproductlist";
+    }
+
+
+    public String getImageBase64(String fileName) {
+        String filePath = System.getProperty("user.dir") + "/sastohubimages/";
+        File file = new File(filePath + fileName);
+        byte[] bytes;
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return Base64.getEncoder().encodeToString(bytes);
+    }
 }
+
