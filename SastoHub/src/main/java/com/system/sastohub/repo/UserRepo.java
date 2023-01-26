3d03afb2c39af2eac4d61adb7fb181b62c20b78e
ImSuis <joshisuyash322@gmail.com>
2023-01-26 21:03:16 +0545

    update query

--- SastoHub/src/main/java/com/system/sastohub/repo/UserRepo.java
+++ SastoHub/src/main/java/com/system/sastohub/repo/UserRepo.java
@@ -10,4 +10,7 @@ public interface UserRepo extends JpaRepository<User,Integer> {
 
     @Query(value="select * from users where email=?1", nativeQuery = true)
     Optional<User> findByEmail(String email);
+
+    @Query(value = "UPDATE users SET password =?1 WHERE email = ?2", nativeQuery = true)
+    void updatePassword(String password, String email);
 }
